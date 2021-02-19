5911227f409e341cf4f763d8c6b9036f1a891f41
Sujit Singh <sujitkumarsingh29@gmail.com>
2021-02-20 01:43:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-const SriPlugin = require("webpack-subresource-integrity");
-const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
@@ -16,45 +14,6 @@ module.exports = (phase, { defaultConfig }) => {
         measurementId: "G-2JJGCYXZDR",
         domain: "http://localhost:3000",
       },
-      async headers() {
-        return [
-          {
-            source: "/(.*)",
-            headers: createSecureHeaders({
-              contentSecurityPolicy: {
-                directives: {
-                  defaultSrc: ["'self'"],
-                  styleSrc: ["'self'", "'unsafe-inline'"],
-                  imgSrc: ["'self'"],
-                  baseUri: "self",
-                  formAction: "self",
-                  frameAncestors: true,
-                },
-              },
-              frameGuard: "deny",
-              noopen: "noopen",
-              nosniff: "nosniff",
-              xssProtection: "sanitize",
-              forceHTTPSRedirect: [
-                true,
-                { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true },
-              ],
-              referrerPolicy: "same-origin",
-            }),
-          },
-        ];
-      },
-      webpack(config) {
-        config.output.crossOriginLoading = "anonymous";
-        config.plugins.push(
-          new SriPlugin({
-            hashFuncNames: ["sha256", "sha384"],
-            enabled: true,
-          })
-        );
-
-        return config;
-      },
     };
   }
 
@@ -69,45 +28,6 @@ module.exports = (phase, { defaultConfig }) => {
       appId: "1:911645212434:web:c81de3a8db41bb2e4dfdcb",
       measurementId: "G-2JJGCYXZDR",
       domain: "https://technovanza.org",
-    },
-    async headers() {
-      return [
-        {
-          source: "/(.*)",
-          headers: createSecureHeaders({
-            contentSecurityPolicy: {
-              directives: {
-                defaultSrc: ["'self'"],
-                styleSrc: ["'self'", "'unsafe-inline'"],
-                imgSrc: ["'self'"],
-                baseUri: "self",
-                formAction: "self",
-                frameAncestors: true,
-              },
-            },
-            frameGuard: "deny",
-            noopen: "noopen",
-            nosniff: "nosniff",
-            xssProtection: "sanitize",
-            forceHTTPSRedirect: [
-              true,
-              { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true },
-            ],
-            referrerPolicy: "same-origin",
-          }),
-        },
-      ];
-    },
-    webpack(config) {
-      config.output.crossOriginLoading = "anonymous";
-      config.plugins.push(
-        new SriPlugin({
-          hashFuncNames: ["sha256", "sha384"],
-          enabled: true,
-        })
-      );
-
-      return config;
     },
   };
 };
