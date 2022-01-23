d2f5d5a16459908f48f01440b824b854c502beb1
Sujit Singh <sujitkumarsingh29@gmail.com>
2022-01-23 12:17:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,13 +7,14 @@ module.exports = (phase, { defaultConfig }) => {
     return {
       /* development only config options here */
       env: {
-        apiKey: "AIzaSyAm855leq4XR4nd-wwMcpgVGmSNDNqV134",
-        authDomain: "technovanza-2020.firebaseapp.com",
-        projectId: "technovanza-2020",
-        storageBucket: "technovanza-2020.appspot.com",
-        messagingSenderId: "911645212434",
-        appId: "1:911645212434:web:c81de3a8db41bb2e4dfdcb",
-        measurementId: "G-2JJGCYXZDR",
+        apiKey: "AIzaSyDIFlrAHUb_xrR4SadMIXkC2oTJ4gLi23I",
+        authDomain: "technovanza-3e853.firebaseapp.com",
+        databaseURL: "https://technovanza-3e853.firebaseio.com",
+        projectId: "technovanza-3e853",
+        storageBucket: "technovanza-3e853.appspot.com",
+        messagingSenderId: "163729170268",
+        appId: "1:163729170268:web:e60535731c1c2aebebeb99",
+        measurementId: "G-6T8LTS2VJT",
         domain: "http://localhost:3000",
       },
       // async headers() {
@@ -103,13 +104,14 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     /* config options for all phases except development here */
     env: {
-      apiKey: "AIzaSyAm855leq4XR4nd-wwMcpgVGmSNDNqV134",
-      authDomain: "technovanza-2020.firebaseapp.com",
-      projectId: "technovanza-2020",
-      storageBucket: "technovanza-2020.appspot.com",
-      messagingSenderId: "911645212434",
-      appId: "1:911645212434:web:c81de3a8db41bb2e4dfdcb",
-      measurementId: "G-2JJGCYXZDR",
+      apiKey: "AIzaSyDIFlrAHUb_xrR4SadMIXkC2oTJ4gLi23I",
+      authDomain: "technovanza-3e853.firebaseapp.com",
+      databaseURL: "https://technovanza-3e853.firebaseio.com",
+      projectId: "technovanza-3e853",
+      storageBucket: "technovanza-3e853.appspot.com",
+      messagingSenderId: "163729170268",
+      appId: "1:163729170268:web:e60535731c1c2aebebeb99",
+      measurementId: "G-6T8LTS2VJT"
       domain: "https://technovanza-sujitsingh11.vercel.app/",
     },
   };
